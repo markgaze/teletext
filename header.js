27050236b77e310af671361fada9a295e44048c4
markgaze <mark.l.gaze@gmail.com>
2018-02-28 23:06:02 +0000

    Use new js syntax

--- header.js
+++ header.js
@@ -2,17 +2,17 @@ const dayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 function addLeadingSpace(number) {
-  return (number < 10 ? " " : "") + number; 
+  return `${(number < 10 ? " " : "")}${number}`; 
 }
 
 function addLeadingZero(number) {
-  return (number < 10 ? "0" : "") + number; 
+  return `${(number < 10 ? "0" : "")}${number}`; 
 }
 
 function updateClock() {
   var d = new Date();
-  document.getElementById("date").innerHTML = dayOfWeek[d.getDay()] + " " + addLeadingSpace(d.getDate()) + " " + monthName[d.getMonth()];
-  document.getElementById("time").innerHTML = addLeadingZero(d.getHours()) + ":" + addLeadingZero(d.getMinutes()) + "/" + addLeadingZero(d.getSeconds());
+  document.getElementById("date").innerHTML = `${dayOfWeek[d.getDay()]} ${addLeadingSpace(d.getDate())} ${monthName[d.getMonth()]}`;
+  document.getElementById("time").innerHTML = `${addLeadingZero(d.getHours())}:${addLeadingZero(d.getMinutes())}/${addLeadingZero(d.getSeconds())}`;
 }
 
 window.onload = () => {
