6d702bf872b62bf32a36ff2743ae81e45a43541c
Mark Gaze <mark.l.gaze@gmail.com>
2018-03-02 10:48:58 +0000

    Fixed linting errors

--- src/components/page/page.test.tsx
+++ src/components/page/page.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Page from './page';
 
-describe("Page", () => {
+describe('Page', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Page />, div);
--- src/components/score/score.test.tsx
+++ src/components/score/score.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Score from './score';
 
-describe("Score", () => {
+describe('Score', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Score homeTeam={'Home Team'} homeTeamScore={4} homeScorers={['Scorer 1 86', 'Scorer 2 45', 'Scorer 3 76']} awayTeam={'Away Team'} awayTeamScore={0} awayScorers={[]} />, div);
