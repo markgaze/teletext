5f09397c2f73aa55087a409f57e91e6c49d4e432
Mark G <mark.l.gaze@gmail.com>
2018-03-03 19:24:09 +0000

    Added page routes and error page

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import './footer.css';
+import { Link } from 'react-router-dom';
 
 export default class Footer extends React.Component {
   render() {
     return (
         <div className="footer">
-          <a href="#" className="red">Next Page</a>
-          <a href="#" className="green">Football</a>
-          <a href="#" className="yellow">Top Sport</a>
-          <a href="#" className="cyan">Sport</a>
+          <Link to="303" className="red">Scores</Link>
+          <Link to="324" className="green">Table</Link>
+          <Link to="#" className="yellow">Top Sport</Link>
+          <Link to="#" className="cyan">Sport</Link>
         </div>
     );
   }
--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -4,6 +4,9 @@ import Header from '../header/header';
 import Banner from '../banner/banner';
 import Results from '../results/results';
 import Footer from '../footer/footer';
+import { Route, Switch } from 'react-router';
+import Table from '../table/table';
+import Error404 from '../error/error';
 
 export default class Page extends React.Component {
   render() {
@@ -12,7 +15,11 @@ export default class Page extends React.Component {
         <Header />
         <Banner />
         <div className="main">
-          <Results />
+          <Switch>
+            <Route path="/303" component={Results} />
+            <Route path="/324" component={Table} />
+            <Route component={Error404} />
+          </Switch>
         </div>
         <Footer />
       </div>
