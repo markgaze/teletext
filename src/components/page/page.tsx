862acc5601403b7ed86754dc02461eeba6c4f8ea
Mark G <mark.l.gaze@gmail.com>
2018-03-03 20:00:57 +0000

    Added basic homepage

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -4,9 +4,10 @@ import Header from '../header/header';
 import Banner from '../banner/banner';
 import Results from '../results/results';
 import Footer from '../footer/footer';
-import { Route, Switch } from 'react-router';
+import { Route, Switch, Redirect } from 'react-router';
 import Table from '../table/table';
 import Error404 from '../error/error';
+import Home from '../home/home';
 
 export default class Page extends React.Component {
   render() {
@@ -16,6 +17,8 @@ export default class Page extends React.Component {
         <Banner />
         <div className="main">
           <Switch>
+            <Route exact={true} path="/" render={() => (<Redirect to="/100" />)} />
+            <Route path="/100" component={Home} />
             <Route path="/303" component={Results} />
             <Route path="/324" component={Table} />
             <Route component={Error404} />
