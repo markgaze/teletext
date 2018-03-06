35ccad69f9d84c5a34f805ccd66318765ec845ff
Mark Gaze <mark.l.gaze@gmail.com>
2018-03-06 15:44:21 +0000

    Updated classes to use new double text generator and modified styles

--- src/components/error/error.tsx
+++ src/components/error/error.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import './error.css';
 import { Link } from 'react-router-dom';
+import DoubleTextGenerator from '../../helpers/double-text';
 
 export default class Error404 extends React.Component {
   render() {
     return (
         <div className="error">
-          <div className="errorText">
-            <span className="yellow blackbg">&#xe034;&#xe030;&#xe034;&#xe03a;&#xe020;&#xe050;&#xe041;&#xe047;&#xe045;&#xe020;&#xe04e;&#xe04f;&#xe054;&#xe020;&#xe046;&#xe04f;&#xe055;&#xe04e;&#xe044;</span>
-            <span className="yellow blackbg">&#xe134;&#xe130;&#xe134;&#xe13a;&#xe120;&#xe150;&#xe141;&#xe147;&#xe145;&#xe120;&#xe14e;&#xe14f;&#xe154;&#xe120;&#xe146;&#xe14f;&#xe155;&#xe14e;&#xe144;</span>
-          </div>
+          {DoubleTextGenerator.CreateText('404 Page not found')}
           <p className="link">Go back to page <Link to="/" className="yellow">100</Link></p>
         </div>
     );
