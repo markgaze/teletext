ad24f911325aea5b3dd56025744c0347190e8fc0
Mark Gaze <mark.l.gaze@gmail.com>
2018-03-08 16:27:33 +0000

    Created basic home page

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -6,28 +6,62 @@ import DoubleTextGenerator from '../../helpers/double-text';
 export default class Home extends React.Component {
   render() {
     return (
-        <div className="container">
-            <Link to="/303">
+      <div className="container">
+        <div className="main-headline">
+          <p className="yellow">News</p>
+          <Link to="/104">
             <div className="option">
-                <div className="dbl">
-                  {DoubleTextGenerator.CreateText('View Football Scores')}
-                </div>
-                <div className="dbl">
-                  {DoubleTextGenerator.CreateText('303', 'cyan')}
-                </div>
-              </div>
-            </Link>
-            <Link to="/324">
-              <div className="option">
-                <div className="dbl">
-                  {DoubleTextGenerator.CreateText('View Football Tables')}
-                </div>
-                <div className="dbl">
-                  {DoubleTextGenerator.CreateText('324', 'cyan')}
-                </div>
-              </div>
-            </Link>
+              {DoubleTextGenerator.CreateText('MAIN NEWS HEADLINE GOES HERE')}
+              {DoubleTextGenerator.CreateText('104', 'yellow')}
+            </div>
+          </Link>
+          <hr className="blue"/>
         </div>
+        <div className="first-column">
+          {this.getOption('A-Z HEADLINES', '199')}
+          {this.getOption('BBC INFO', '695')}
+          {this.getOption('CHILDREN', '500')}
+          {this.getOption('COOKERY', '580')}
+          {this.getOption('COMMUNITY', '650')}
+          <div className="spacer" />
+          {this.getOption('EDUCATION', '630')}
+          {this.getOption('ENTERTAINMENT', '540')}
+          {this.getOption('FILMS', '570')}
+          {this.getOption('FINANCE', '200')}
+          {this.getOption('HORSERACING', '660')}
+          {this.getOption('LOTTERY', '555')}
+          {this.getOption('MUSIC', '560')}
+        </div>
+        <div className="second-column">
+          {this.getOption('NEWS HEADLINES', '101')}
+          {this.getOption('NEWS FOR REGION', '160')}
+          {this.getOption('NEWSROUND', '510')}
+          {this.getOption('RADIO', '640')}
+          {this.getOption('READ HEAR', '641')}
+          <div className="spacer" />
+          {this.getOption('SPORT', '300')}
+          {this.getOption('SUBTITLING', '888')}
+          {this.getOption('TRAVEL', '430')}
+          {this.getOption('TV FEATURES', '590')}
+          {this.getOption('TV LINKS', '615')}
+          {this.getOption('TV LISTINGS', '600')}
+          {this.getOption('WEATHER', '400')}
+        </div>
+        <div className="page-footer bluebg">
+          <p className="yellow">Some interesting text goes here</p>
+        </div>
+      </div>
+    );
+  }
+
+  getOption(text: string, pageNum: string) {
+    return (
+      <Link className="white" to={pageNum}>
+        <div className="option">
+          <p className="yellow">{text}</p>
+          <p>{pageNum}</p>
+        </div>
+      </Link>
     );
   }
 }
