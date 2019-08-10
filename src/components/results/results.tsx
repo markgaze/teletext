199cc46500246534e74c89cab9ad2c6a73b2da03
markgaze <mark.l.gaze@gmail.com>
2019-08-10 19:34:43 +0100

    Add pagination to results

--- src/components/results/results.tsx
+++ src/components/results/results.tsx
@@ -7,14 +7,21 @@ import FootballAPI from '../../services/football/football-data';
 export default class Results extends React.Component<{}, ResultsState> {
   constructor(props: {}) {
     super(props);
-    this.state = { games: [] };
+    this.state = { games: [], page: 1 };
   }
+
   componentWillMount() {
-    new FootballAPI().getThisWeeksGames(2016).then(scores => this.setState({ games: scores }));
+    new FootballAPI().getThisWeeksGames(2016).then(games =>
+      this.setState({
+        page: 1,
+        games
+      })
+    );
+    setInterval(this.changePage.bind(this), 10000);
   }
 
   render() {
-    var games = this.state.games.map(score => 
+    var games = this.getGames(this.state.games).map(score => 
       <Score key={score.homeTeam} scoreData={score} />
     );
 
@@ -25,8 +32,22 @@ export default class Results extends React.Component<{}, ResultsState> {
         </div>
     );
   }
+
+  private changePage(): void {
+    let _page = 1;
+    if (this.state.page === 1) {
+      _page = 2;
+    }
+    this.setState({ page: _page });
+  }
+
+  private getGames(games: ScoreModel[]): ScoreModel[] {
+    const half = games.length / 2;
+    return games.slice(half * (this.state.page - 1), half * this.state.page);
+  }
 }
 
 interface ResultsState {
   games: ScoreModel[];
+  page: number;
 }
