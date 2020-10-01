4e3e4407651d0e7c19d3f621912fef8189540cbd
markgaze <mark.l.gaze@gmail.com>
2020-10-01 14:18:51 +0100

    Make the league table work

--- src/routes/324.svelte
+++ src/routes/324.svelte
@@ -1,51 +1,77 @@
+<script context="module" lang="ts">
+  export async function preload(page: any, session: any) {
+    const authToken: string = '6ef3af956d7f4c6e9db971d4fe244334';
+    const url: string = 'https://api.football-data.org/v2';
+    const compId: string = "2016";
+
+    const res = await this.fetch(`${url}/competitions/${compId}/standings`, {
+      headers: {
+        'X-Auth-Token': authToken
+      }
+    });
+    const standings = await res.json();
+    let teams = standings.standings[0].table.map((t: any) => {
+      return {
+        position: t.position,
+        teamName: t.team.name.replace(' FC', '').replace(' AFC', ''),
+        played: t.playedGames,
+        won: t.won,
+        drawn: t.draw,
+        lost: t.lost,
+        for: t.goalsFor,
+        against: t.goalsAgainst,
+        points: t.points
+      };
+    });
+
+    return {
+      lastUpdated: new Date(standings.competition.lastUpdated),
+      table: teams
+    };
+  }
+</script>
+
 <script lang="ts">
   import dayjs from 'dayjs';
-  import type { ITeam } from '../../data/ITeam';
+  import { onDestroy } from 'svelte';
+  import type { ITeam } from '../data/ITeam';
 
-  let rows: ITeam[] = [
-    {
-      teamName: "Sheffield Wednesday",
-      points: 100,
-      won: 30,
-      drawn: 10,
-      lost: 6,
-      played: 46,
-      against: 50,
-      for: 100,
-      position: 1
-    },
-    {
-      teamName: "Sheffield Wednesday",
-      points: 100,
-      won: 30,
-      drawn: 10,
-      lost: 6,
-      played: 46,
-      against: 50,
-      for: 100,
-      position: 2
-    }
-  ];
+  export let table: ITeam[];
+  export let lastUpdated: Date;
+
   let page: number = 1;
+  let rows: ITeam[] = [];
+  let half: number;
+  
+  $: half = table.length / 2;
+  $: rows = table.slice(half * (page - 1), half * page);
+
+  const interval = setInterval(() => page = (page === 1 ? 2 : 1), 10000);
+
+  onDestroy(() => clearInterval(interval));
 </script>
 
 <style>
+  .table {
+    text-align: right;
+  }
+
   .title {
-    display: grid;
-    grid-template-columns: auto 70px;
+    display: flex;
+    justify-content: space-between;
   }
 
   .table-header {
     padding-top: 3vh;
     padding-bottom: 3vh;
     display: grid;
-    grid-template-columns: auto repeat(7, 50px);
+    grid-template-columns: auto repeat(6, 35px) 50px;
     grid-gap: 10px;
   }
 
   .table-row {
     display: grid;
-    grid-template-columns: 1fr auto repeat(7, 1fr);
+    grid-template-columns: 50px auto repeat(6, 35px) 50px;
     grid-gap: 10px;
   }
 
@@ -53,8 +79,12 @@
     color: #00ffff;
   }
 
-  .right-align {
+  .center-align {
     text-align: center;
+  }
+
+  .left-align {
+    text-align: left;
   }
 </style>
 
@@ -63,27 +93,29 @@
     <p class="green">SKYBET CHAMPIONSHIP</p>
     <p>{page}/2</p>
   </div>
-  <div class="table-header">
-    <p>{dayjs().format('MMM DD')}</p>
-    <p>P</p>
-    <p>W</p>
-    <p>D</p>
-    <p>L</p>
-    <p>F</p>
-    <p>A</p>
-    <p>Pts</p>
-  </div>
-  {#each rows as row}
-    <div class="table-row">
-      <p class="right-align">{row.position}</p>
-      <p>{row.teamName.substring(0, 10)}</p>
-      <p>{row.played}</p>
-      <p>{row.won}</p>
-      <p>{row.drawn}</p>
-      <p>{row.lost}</p>
-      <p>{row.for}</p>
-      <p>{row.against}</p>
-      <p class="right-align">{row.points}</p>
+  <div class="table">
+    <div class="table-header">
+      <p class="left-align">{dayjs(lastUpdated).format('MMM DD')}</p>
+      <p>P</p>
+      <p>W</p>
+      <p>D</p>
+      <p>L</p>
+      <p>F</p>
+      <p>A</p>
+      <p>PTS</p>
     </div>
-  {/each}
+    {#each rows as row}
+      <div class="table-row">
+        <p class="center-align">{row.position}</p>
+        <p class="left-align">{row.teamName.substring(0, 16)}</p>
+        <p>{row.played}</p>
+        <p>{row.won}</p>
+        <p>{row.drawn}</p>
+        <p>{row.lost}</p>
+        <p>{row.for}</p>
+        <p>{row.against}</p>
+        <p>{row.points}</p>
+      </div>
+    {/each}
+  </div>
 </div>
